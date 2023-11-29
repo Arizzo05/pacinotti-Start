b3f36654971f8fe8ead4fd79728ea44f20a516e8
Alessandro Rizzo <152413343+Arizzo05@users.noreply.github.com>
2023-11-29 16:22:35 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
+import Education from "./components/Education";
 //Dichiarazione Styled Components:
 const Div = styled.div`
-  margin: 100px auto;
+  margin: 30px auto;
   padding: 50px;
-  background-color: #66A182;
-  border: 2px solid #2E4057;
+  background-color: brown;
+  border: 2px solid #2e4057;
   max-width: 700px;
   border-radius: 5px;
   display: flex;
@@ -14,19 +15,20 @@ const Div = styled.div`
 `;
 
 const H1 = styled.h1`
-  color: #2E4057;
-  margin: auto;
+  color: white;
+  margin; auto;
   text-align: center;
   font-size: 70px;
 `;
 
 const App = () => {
-
-  return(
+  return (
     <Div>
-      <H1>Hello world</H1>
+      <H1>Benvenuti</H1>
+      <H1></H1>
+      <Education />
     </Div>
-  )
+  );
 };
 
 export default App;
